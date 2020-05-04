31c95a34201b671eaeef8bf76d5390f480773d97
Yuki MIZUNO <mizuyuu0904@gmail.com>
2020-05-04 17:09:14 +0100

    Make Controller struct

--- main.go
+++ main.go
@@ -30,9 +30,37 @@ var SPI_ROM_DATA = map[byte][]byte{
 	},
 }
 
-var count uint8
+type ButtonMap struct {
+	Dpad struct {
+		Up, Down, Left, Right uint8
+	}
+	Button struct {
+		A, B, X, Y, R, ZR, L, ZL   uint8
+		Home, Plus, Minus, Capture uint8
+	}
+	Stick struct {
+		Left, Right struct {
+			X, Y float64
+		}
+	}
+}
 
-func incremental(stop chan struct{}) {
+type Controller struct {
+	fp          *os.File
+	count       uint8
+	stopCounter chan struct{}
+	stopInput   chan struct{}
+	Button      ButtonMap
+}
+
+// Close closes all channel and device file
+func (c *Controller) Close() {
+	close(c.stopCounter)
+	close(c.stopInput)
+	c.fp.Close()
+}
+
+func (c *Controller) startCounter() {
 	ticker := time.NewTicker(time.Millisecond * 5)
 
 	go func() {
@@ -40,17 +68,15 @@ func incremental(stop chan struct{}) {
 		for {
 			select {
 			case <-ticker.C:
-				count++
-			case <-stop:
+				c.count++
+			case <-c.stopCounter:
 				return
 			}
 		}
 	}()
 }
 
-var up, left, right, down uint8
-
-func inputResponse(fp *os.File, stop chan struct{}) {
+func (c *Controller) startInputReport() {
 	ticker := time.NewTicker(time.Millisecond * 30)
 
 	go func() {
@@ -58,84 +84,76 @@ func inputResponse(fp *os.File, stop chan struct{}) {
 		for {
 			select {
 			case <-ticker.C:
-				dpad := left<<3 | right<<2 | up<<1 | down
-				write(fp, []byte{0x30, count, 0x81, 0x00, 0x80, dpad, 0x00, 0x08,
+				dpad := c.Button.Dpad.Left<<3 |
+					c.Button.Dpad.Right<<2 |
+					c.Button.Dpad.Up<<1 |
+					c.Button.Dpad.Down
+				c.write([]byte{0x30, c.count, 0x81, 0x00, 0x80, dpad, 0x00, 0x08,
 					0x80, 0x00, 0x08, 0x80, 0x00})
-			case <-stop:
+			case <-c.stopInput:
 				return
 			}
 		}
 	}()
 }
 
-func uart(fp *os.File, ack byte, subCmd byte, data []byte) {
-	write(fp, append([]byte{0x21, count, 0x81, 0x00, 0x80, 0x00, 0x00, 0x08,
+func (c *Controller) uart(ack byte, subCmd byte, data []byte) {
+	c.write(append([]byte{0x21, c.count, 0x81, 0x00, 0x80, 0x00, 0x00, 0x08,
 		0x80, 0x00, 0x08, 0x80, 0x00, ack, subCmd}, data...))
 }
 
-func write(fp *os.File, buf []byte) {
+func (c *Controller) write(buf []byte) {
 	data := append(buf, make([]byte, 64-len(buf))...)
-	fp.Write(data)
+	c.fp.Write(data)
 	if buf[0] != 0x30 {
 		log.Println("write:", hex.EncodeToString(data))
 	}
 }
 
-func main() {
-	target := "/dev/hidg0"
-
-	fp, err := os.OpenFile(target, os.O_RDWR|os.O_SYNC, os.ModeDevice)
-
-	if err != nil {
-		panic(err)
-	}
-	defer fp.Close()
-
-	stopCounter := make(chan struct{})
-	stopInput := make(chan struct{})
-	incremental(stopCounter)
-
+// Connect begins connection to device
+func (c *Controller) Connect() {
+	c.startCounter()
 	go func() {
 		buf := make([]byte, 128)
 
 		for {
-			n, err := fp.Read(buf)
+			n, err := c.fp.Read(buf)
 			log.Println("read:", hex.EncodeToString(buf[:n]), err)
 			switch buf[0] {
 			case 0x80:
 				switch buf[1] {
 				case 0x01:
-					write(fp, []byte{0x81, buf[1], 0x00, 0x03, 0x00, 0x00, 0x5e, 0x00, 0x53, 0x5e})
+					c.write([]byte{0x81, buf[1], 0x00, 0x03, 0x00, 0x00, 0x5e, 0x00, 0x53, 0x5e})
 				case 0x02, 0x03:
-					write(fp, []byte{0x81, buf[1]})
+					c.write([]byte{0x81, buf[1]})
 				case 0x04:
-					inputResponse(fp, stopInput)
+					c.startInputReport()
 				case 0x05:
-					close(stopInput)
-					stopInput = make(chan struct{})
+					close(c.stopInput)
+					c.stopInput = make(chan struct{})
 				}
 			case 0x01:
 				switch buf[10] {
 				case 0x01: // Bluetooth manual pairing
-					uart(fp, 0x81, buf[10], []byte{0x03, 0x01})
+					c.uart(0x81, buf[10], []byte{0x03, 0x01})
 				case 0x02: // Request device info
-					uart(fp, 0x82, buf[10], []byte{0x03, 0x48, 0x03,
+					c.uart(0x82, buf[10], []byte{0x03, 0x48, 0x03,
 						0x02, 0x5e, 0x53, 0x00, 0x5e, 0x00, 0x00, 0x03, 0x01})
 				case 0x03, 0x08, 0x30, 0x38, 0x40, 0x41, 0x48: // Empty response
-					uart(fp, 0x80, buf[10], []byte{})
+					c.uart(0x80, buf[10], []byte{})
 				case 0x04: // Empty response
-					uart(fp, 0x80, buf[10], []byte{})
+					c.uart(0x80, buf[10], []byte{})
 				case 0x10: // Read SPI ROM
 					data, ok := SPI_ROM_DATA[buf[12]]
 					if ok {
-						uart(fp, 0x90, buf[10], append(buf[11:16],
+						c.uart(0x90, buf[10], append(buf[11:16],
 							data[buf[11]:buf[11]+buf[15]]...))
 						log.Printf("Read SPI address: %02x%02x[%d] %v\n", buf[12], buf[11], buf[15], data[buf[11]:buf[11]+buf[15]])
 					} else {
 						log.Printf("Unknown SPI address: %02x[%d]\n", buf[12], buf[15])
 					}
 				case 0x21:
-					uart(fp, 0xa0, buf[10], []byte{0x01, 0x00, 0xff, 0x00, 0x03, 0x00, 0x05, 0x01})
+					c.uart(0xa0, buf[10], []byte{0x01, 0x00, 0xff, 0x00, 0x03, 0x00, 0x05, 0x01})
 				default:
 					log.Println("UART unknown request", buf[10], buf)
 				}
@@ -147,6 +165,18 @@ func main() {
 			}
 		}
 	}()
+}
+
+func main() {
+	target := "/dev/hidg0"
+
+	fp, err := os.OpenFile(target, os.O_RDWR|os.O_SYNC, os.ModeDevice)
+	if err != nil {
+		panic(err)
+	}
+	con := Controller{fp: fp, stopInput: make(chan struct{}), stopCounter: make(chan struct{})}
+	defer con.Close()
+	con.Connect()
 
 	buf := make([]byte, 1)
 
@@ -158,20 +188,20 @@ func main() {
 		os.Stdin.Read(buf)
 		switch buf[0] {
 		case 0x61:
-			left = 1
+			con.Button.Dpad.Left = 1
 		case 0x64:
-			right = 1
+			con.Button.Dpad.Right = 1
 		case 0x77:
-			up = 1
+			con.Button.Dpad.Up = 1
 		case 0x73:
-			down = 1
+			con.Button.Dpad.Down = 1
 		default:
 			log.Printf("unknown: %c = 0x%02x\n", buf[0], buf[0])
 		}
 		time.Sleep(50 * time.Millisecond)
-		left = 0
-		right = 0
-		up = 0
-		down = 0
+		con.Button.Dpad.Left = 0
+		con.Button.Dpad.Right = 0
+		con.Button.Dpad.Up = 0
+		con.Button.Dpad.Down = 0
 	}
 }
